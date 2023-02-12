20643969eb9bf541dbe9ffc6510340a5e19f6254
Stiven M <stvnm33@gmail.com>
2023-02-12 11:11:34 -0500

    Adding vehicles

--- src/layouts/PrivateLayout.jsx
+++ src/layouts/PrivateLayout.jsx
@@ -4,6 +4,7 @@ import Sidebar from '../components/Sidebar'
 const PrivateLayout = ({children}) => {
   return (
     <div className='flex w-screen h-screen '>
+      
       <Sidebar></Sidebar>
       <main className='flex w-full bg-blue-400'><Outlet></Outlet></main>
     </div>
--- src/pages/Vehiculos.jsx
+++ src/pages/Vehiculos.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Vehiculos = () => {
+
+    const [form, setForm] = useState({nombre: '', marca: '', modelo: ''})
+
+    const [boton, setBoton] = useState({estado: true, texto: 'Crear  vehiculo'})
+
+    useEffect(() => {
+      if(boton.estado = true){
+        setBoton({...boton, texto:'Crear vehiculo'})
+      }else{
+        setBoton({...boton, texto:'Mostrar vehiculos'})
+      }
+    }, [boton])
+    
+    
   return (
     <div>
-      🚗🚗🚗
+        <h2>Creacion de Vehiculos</h2>
+        <button onClick={()=>{setBoton({...boton, estado:!boton.estado})}} className='bg-indigo-400'>{boton.texto}</button>
+        <form action="">
+            <input onChange={(e)=>{setForm({...form, nombre:e.target.value})}} type="text" placeholder='nombre vehiculo' />
+            <input onChange={(e)=>{setForm({...form, marca:e.target.value})}} type="text" placeholder='Marca del vehiculo' />
+            <input onChange={(e)=>{setForm({...form, modelo:e.target.value})}} type="text" placeholder='Modelo del vehiculo' />
+            <button className='bg-indigo-700' type="submit">Enviar datos</button>
+        </form>
     </div>
   )
 }
